0e90cba8f24d09fb08049bf0c19b093f8201910e
alikemal <alikemalocalan@gmail.com>
2018-06-03 15:27:45 +0300

    Fix minor bugs

---
+++ src/main/java/com/alikemal/codestat/Config.java
@@ -0,0 +1,9 @@
+package com.alikemal.codestat;
+
+public class Config {
+    public final static String DEFAULT_API_URL = "https://codestats.net/api/my/pulses";
+    public final static String CONFIG_PREFIX = "code-stats-intellij-";
+    public final static String API_KEY_NAME = "SFMyNTY.WVd4cGEyVnRZV3h2WTJGc1lXND0jI01qVTBNdz09.xjLuMikaZIT73icNsNithCMXzH0w5HI9nVFJSxQloJ0";
+    public final static String VERSION = "unknown";
+    public final static long UPDATE_TIMER = 10;
+}
--- src/main/java/com/alikemal/codestat/UpdateTask.java
+++ src/main/java/com/alikemal/codestat/UpdateTask.java
@@ -3,9 +3,8 @@ package com.alikemal.codestat;
 
 import com.alikemal.codestat.model.XP;
 import com.alikemal.codestat.model.XpResponse;
-import com.alikemal.codestat.ui.SettingsForm;
 import com.alikemal.codestat.ui.StatusBarIcon;
-import com.google.gson.Gson;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.wm.IdeFrame;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -14,80 +13,67 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.apache.http.util.EntityUtils;
+import org.apache.log4j.Level;
 
 import java.io.IOException;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 import java.util.TimeZone;
 
 /**
- * Task that sends the update to the Code::Stats servers.
+ * Task that seds the update to the Code::Stats servers.
  */
 
 public class UpdateTask implements Runnable {
+    private Logger log = Logger.getInstance(UpdateTask.class);
 
-    private Hashtable<String, Integer> xps;
-    private Object xpsLock;
-    private String apiURL;
-    private String apiKey;
-    private Hashtable<IdeFrame, StatusBarIcon> statusBarIcons;
-    private String version = "unknown";
+    private Map<String, Integer> xps;
+    private Map<IdeFrame, StatusBarIcon> statusBarIcons;
 
-    public UpdateTask() {
+    public UpdateTask(Map<String, Integer> xps, Map<IdeFrame, StatusBarIcon> statusBarIcons) {
+        this.xps = xps;
+        this.statusBarIcons = statusBarIcons;
     }
+
 
     @Override
     public void run() {
+        log.setLevel(Level.DEBUG);
         statusBarIcons.values().forEach(StatusBarIcon::setUpdating);
 
+        List<XP> xpList = new ArrayList<>();
+
+        xps.forEach((s, integer) -> xpList.add(new XP(s, integer)));
+
+        final String now = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX")
+                .withZone(TimeZone.getDefault().toZoneId())
+                .format(Instant.now());
+
+        HttpResponse response = null;
         try {
-            List<XP> xpList = new ArrayList<>();
+            StringEntity entity = new StringEntity(new XpResponse(now, xpList).toString());
 
-            xps.forEach((s, integer) -> xpList.add(new XP(s, integer)));
-
-            final String now = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX")
-                    .withZone(TimeZone.getDefault().toZoneId())
-                    .format(Instant.now());
-
-            StringEntity entity = new StringEntity(new Gson().toJson(new XpResponse(now, xpList)));
-
-            // Sometimes after some combination of user action, the API URL ends up as an empty string, guard against it
-            if (apiURL == null || apiURL.equals("")) {
-                apiURL = SettingsForm.DEFAULT_API_URL;
-            }
-
-            final String API_TOKEN = apiKey;
-
-            HttpPost postMethod = new HttpPost(apiURL);
+            HttpPost postMethod = new HttpPost(Config.DEFAULT_API_URL);
             postMethod.addHeader("Content-Type", "application/json");
-            postMethod.addHeader("X-API-Token", API_TOKEN);
-            postMethod.addHeader("User-Agent", "code-stats-intellij/" + version);
+            postMethod.addHeader("X-API-Token", Config.API_KEY_NAME);
+            postMethod.addHeader("User-Agent", Config.CONFIG_PREFIX + Config.VERSION);
             postMethod.setEntity(entity);
 
             CloseableHttpClient client = HttpClientBuilder.create().setRedirectStrategy(new LaxRedirectStrategy()).build();
-            HttpResponse response = null;
-            try {
-                response = client.execute(postMethod);
-                System.out.println(response.getStatusLine().getStatusCode());
-                System.out.println(EntityUtils.toString(response.getEntity()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    client.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            response = client.execute(postMethod);
+            log.debug(Integer.toString(response.getStatusLine().getStatusCode()));
+            log.debug(EntityUtils.toString(response.getEntity()));
+            client.close();
+        } catch (IOException e) {
+            log.error(e);
+        }
 
+        if (response != null) {
             if (response.getStatusLine().getStatusCode() == 201) {
-                synchronized (xpsLock) {
-                    xps.clear();
-                }
-
+                xps.clear();
                 statusBarIcons.values().forEach(StatusBarIcon::clear);
             } else if (response.getStatusLine().getStatusCode() == 403) {
                 // The API key was wrong when updating
@@ -96,37 +82,14 @@ public class UpdateTask implements Runnable {
                 }
             } else {
                 for (StatusBarIcon statusBarIcon : statusBarIcons.values()) {
-                    statusBarIcon.setError("Unknown status code when updating: " + String.valueOf(response.getStatusLine().getStatusCode()));
+                    statusBarIcon.setError("Unknown status code when updating: " + response.getStatusLine().getStatusCode());
                 }
             }
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        } else {
             for (StatusBarIcon statusBarIcon : statusBarIcons.values()) {
-                statusBarIcon.setError(e.toString());
+                statusBarIcon.setError("Connection timeout error");
+                log.error("Connection timeout error");
             }
         }
-    }
-
-    public void setXps(Hashtable<String, Integer> xps) {
-        this.xps = xps;
-    }
-
-    public void setXpsLock(final Object xpsLock) {
-        this.xpsLock = xpsLock;
-    }
-
-    public void setConfig(final String apiURL, final String apiKey) {
-        this.apiURL = apiURL;
-        this.apiKey = apiKey;
-    }
-
-    public void setStatusBarIcons(Hashtable<IdeFrame, StatusBarIcon> statusBarIcons) {
-        this.statusBarIcons = statusBarIcons;
-    }
-
-
-    public void setVersion(String version) {
-        this.version = version;
     }
 }
